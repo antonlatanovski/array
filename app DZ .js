e7bb20ebadc906641f67bb53ed4ce43b732a1341
Anton Latanovski <antonlatanovski@gmail.com>
2024-01-29 15:06:24 +0300

    function

--- app DZ .js
+++ app DZ .js
@@ -1,30 +1,39 @@
-//  Задание 8
-//  let arr =[];
-//  let sum = 0;
-//  for( let i =0; i<100;i++){
-//      arr[i]=Math.floor(Math.random()*100)   
-//     if(arr[i]%5==0){
-//          sum= (sum+arr[i])/2
-//      }
-//  }alert(Math.floor(sum))
-
-// Задание 10
-// let arr =[];
-// let sum = [];
-// for( let i =0; i<100;i++){
-//     arr[i]=Math.floor(Math.random()*100)   
-//     if(arr[i]%10==0){
-//       sum.push(arr[i])
-//     }
-// }alert(sum.length)
+// Задача 1.
+// function sum (a,b){
+    // return sum=a+b
+// } alert(sum(5,20))
 
 
- Задание 9
-let arr =[];
-let min = 0;
-let max = 0;
-for( let i =0; i<10;i++){
-    arr[i]=Math.floor(Math.random()*100)   
-   
-}alert(Math.max(...arr)) 
-alert(Math.min(...arr))
+// Зфдание 2.
+// let arr=[5,10,15,20,25];
+// function newArr(arr){
+    // let sum=0;
+    // for(let i=0; i<arr.length;i++){
+        // sum=arr[i]+sum;
+       
+    // } return newArr=sum/arr.length;
+// } alert(newArr(arr))
+
+// Зфдание 3.
+// let str="JavaScript";
+// function revStr(str){
+    // let strRew = " ";
+    // for(let i=str.length-1;i>=0;i--){
+        // strRew=str[i]+strRew
+    // } return revStr(str)=== strRew;
+//  } alert(revStr(str));
+
+ // Зфдание 3.
+
+function revStr(str){
+  return  str.split("").reverse().join("");
+ } alert(revStr("JavaScript"));
+
+
+ // Зфдание 4.
+// function num(a){
+//    if( a===Math.floor(a)){
+    // return true;}
+//    else{
+    // return false;} 
+// } alert(num(5))
