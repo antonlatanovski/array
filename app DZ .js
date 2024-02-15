d51ff1ad1c2f66c701161f562b743a338920cf64
Anton Latanovski <antonlatanovski@gmail.com>
2024-02-15 19:36:52 +0300

    OOP

--- app DZ .js
+++ app DZ .js
@@ -1,50 +1,64 @@
-// Задача 1.
-// function sum (a,b){
-    // return sum=a+b
-// } alert(sum(5,20))
+// Задание 1.
+// function BankAccound(balance, summ){
+    // this.balance = balance
+    // this.summ = summ
+    // this.plusSumm = function(){
+        // alert(this.balance+this.summ)
+    // }
+    // this.withdrawalSumm = function(){
+        // alert(this.balance-this.summ)
+    // }
+    // this.loocBalance = function(){
+        // alert(this.balance)
+    // }
+// } 
+//  const BankAccound1 = new BankAccound( 10, 20)
+//  BankAccound1.plusSumm()
 
+// Задание 2.
+//  function Calculator(num1, num2){
+    // this.num1 = num1
+    // this.num2= num2
+    //  this.plusSumm = function(){
+        // alert(this.num1+this.num2)
+    //  }
+    //  this.subtraction = function(){
+        //  alert(this.num1-this.num2)
+    //  }
+    // this.multiply = function(){
+        //  alert(this.num1*this.num2)
+    //  }
+    //  this.divide = function(){
+        // alert(this.num1/this.num2)
+    // }
+//    } 
+//   const Calculator1 = new Calculator( 30, 20)
+//  Calculator1.subtraction()
 
-// Зфдание 2.
-// let arr=[5,10,15,20,25];
-// function newArr(arr){
-    // let sum=0;
-    // for(let i=0; i<arr.length;i++){
-        // sum=arr[i]+sum;
-       
-    // } return sum/arr.length;
-// } alert(newArr(arr))
+// Задание 2-2.
+//  function Circle(radius){
+    // this.radius = radius
+    // this.area = function(){
+        // alert(3.14*(Math.pow(this.radius,2)))
+    //  }
+    //  this.lengt = function(){
+        //  alert(2*3,14*this.radius)
+    //  }   
+//    } 
+//   const Circle1 = new Circle( 10)
+//  Circle1.area()
+//  Circle1.lengt()
 
-// Зфдание 3.
-
-//  function revStr(str){
-    //  let strNew = " ";
-//  for(let i=str.length-1;i>=0;i--){
-        //  strNew=strNew+str[i]
-    //  } return  strNew;
-//  } alert(revStr(str));
-
- // Зфдание 3.
-
-// function revStr(str){
-//   return  str.split("").reverse().join("");
-//  } alert(revStr("JavaScript"));
-
-
- // Зфдание 4.
-// function num(a){
-//    if( a===Math.floor(a)){
-    // return true;}
-//    else{
-    // return false;} 
-// } alert(num(5))
-
-// Задание 5.
-
- function revStr(str,ch){  
-    let sum=0; 
-for(let i=0; i<str.length;i++){
-        if(str[i]===ch){
-            sum++;
-        }
-     } return  sum;
- } alert(revStr('hello','l'));
+// Задание 2-3.
+ function  Employee(name, position,salary,interest){
+    this.name = name
+    this.position = position
+    this.salary = salary
+    this.interest = interest
+    this.applyBonus = function(){
+        alert(this.salary*this.interest)
+       } 
+ }
+  const Employee1 = new Employee(" 1a","b",1000,2)
+ Employee1.applyBonus()
+ 
