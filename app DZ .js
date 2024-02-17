7a8904c20a1a83da0e31692efe51dec8c3d3a2e4
Anton Latanovski <antonlatanovski@gmail.com>
2024-02-17 13:50:52 +0300

    OOP List

--- app DZ .js
+++ app DZ .js
@@ -1,64 +1,46 @@
-// Задание 1.
-// function BankAccound(balance, summ){
-    // this.balance = balance
-    // this.summ = summ
-    // this.plusSumm = function(){
-        // alert(this.balance+this.summ)
-    // }
-    // this.withdrawalSumm = function(){
-        // alert(this.balance-this.summ)
-    // }
-    // this.loocBalance = function(){
-        // alert(this.balance)
-    // }
-// } 
-//  const BankAccound1 = new BankAccound( 10, 20)
-//  BankAccound1.plusSumm()
-
-// Задание 2.
-//  function Calculator(num1, num2){
-    // this.num1 = num1
-    // this.num2= num2
-    //  this.plusSumm = function(){
-        // alert(this.num1+this.num2)
-    //  }
-    //  this.subtraction = function(){
-        //  alert(this.num1-this.num2)
-    //  }
-    // this.multiply = function(){
-        //  alert(this.num1*this.num2)
-    //  }
-    //  this.divide = function(){
-        // alert(this.num1/this.num2)
-    // }
-//    } 
-//   const Calculator1 = new Calculator( 30, 20)
-//  Calculator1.subtraction()
-
-// Задание 2-2.
-//  function Circle(radius){
-    // this.radius = radius
-    // this.area = function(){
-        // alert(3.14*(Math.pow(this.radius,2)))
-    //  }
-    //  this.lengt = function(){
-        //  alert(2*3,14*this.radius)
-    //  }   
-//    } 
-//   const Circle1 = new Circle( 10)
-//  Circle1.area()
-//  Circle1.lengt()
 
 // Задание 2-3.
- function  Employee(name, position,salary,interest){
-    this.name = name
-    this.position = position
-    this.salary = salary
-    this.interest = interest
-    this.applyBonus = function(){
-        alert(this.salary*this.interest)
-       } 
- }
-  const Employee1 = new Employee(" 1a","b",1000,2)
- Employee1.applyBonus()
  
+// function  Product(cost, title){
+    this.cost = cost
+    this.title = title
+
+//   } 
+//   function ShoopingList(){
+    //  let products = []
+    //  this.addProduct= function(product){ 
+        // products.push(product)
+    //  } 
+    //  this.deliteProduct=function(product){
+    //    let indexProduct = products.indexOf(product)
+        // products.splice(indexProduct,1)
+    //  }
+    //  this.sumCostProduct=function(sum){
+        // let sum =0;       
+        // for(let i=0; i%2; i<products.length){
+                //    sum=sum+products[i];   
+        // }
+    //  }
+//   }
+// const ShoopingList1 = new ShoopingList()
+// const Product1 = new Product( 100, "apple")
+// const Product2 = new Product(220, "tools")
+// ShoopingList1.addProduct(Product1)
+// ShoopingList1.sumCostProduct()
+
+function  WebHistory(url){
+ 
+}
+function UrlList() { 
+   let urlPage = []
+   this.addUrlPage = function(url){
+  urlPage.push(url)
+
+ 
+   }
+ }
+  const WebHistory1 = new WebHistory("hhp")
+  const WebHistory2 = new WebHistory("hhpw")
+  const UrlList1 = new UrlList()
+  UrlList1.addUrlPage(WebHistory1, WebHistory2)
+  alert(urlPage)
