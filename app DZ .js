a6bda50228e2bcaf82f9ca374ff3482641e230fa
Anton Latanovski <antonlatanovski@gmail.com>
2024-02-22 14:25:32 +0300

    encapsulation

--- app DZ .js
+++ app DZ .js
@@ -1,46 +1,104 @@
+function BankAccount (){
+    let balans_=0
+    this.addBalans=function(sum) { 
+       return balans_+sum
+     }
+     this.withdrawBalans= function(sum){
+       if(sum<=balans_){
+      this.balans_=balans_-sum
+       }
+     }
+    this.getBalans = function (){
+       return balans_
+     }
+     this.setBalans= function(balans){
+      if(balans>=0){
+        this.balans_ = balans
+        return balans
+      } 
+     }
+   } 
+   const BankAccount1 = new BankAccount()
+  alert(BankAccount1.setBalans(1000)) 
+alert(BankAccount1.addBalans(20))
+alert(BankAccount1.getBalans())
 
-// Задание 2-3.
- 
-// function  Product(cost, title){
-    this.cost = cost
-    this.title = title
-
-//   } 
-//   function ShoopingList(){
-    //  let products = []
-    //  this.addProduct= function(product){ 
-        // products.push(product)
-    //  } 
-    //  this.deliteProduct=function(product){
-    //    let indexProduct = products.indexOf(product)
-        // products.splice(indexProduct,1)
-    //  }
-    //  this.sumCostProduct=function(sum){
-        // let sum =0;       
-        // for(let i=0; i%2; i<products.length){
-                //    sum=sum+products[i];   
-        // }
-    //  }
-//   }
-// const ShoopingList1 = new ShoopingList()
-// const Product1 = new Product( 100, "apple")
-// const Product2 = new Product(220, "tools")
-// ShoopingList1.addProduct(Product1)
-// ShoopingList1.sumCostProduct()
-
-function  WebHistory(url){
- 
+// Задание 1
+function User (name, age){
+  let name_=name
+  let age_=age
+  this.getUserName=function(name){  
+    return name
+  }
+  this.getUserAge=function(age){  
+    return age
+  }
 }
-function UrlList() { 
-   let urlPage = []
-   this.addUrlPage = function(url){
-  urlPage.push(url)
+const User1 = new User()
+alert(User1.getUserName("Antun"))
 
- 
-   }
- }
-  const WebHistory1 = new WebHistory("hhp")
-  const WebHistory2 = new WebHistory("hhpw")
-  const UrlList1 = new UrlList()
-  UrlList1.addUrlPage(WebHistory1, WebHistory2)
-  alert(urlPage)
+// Задание 2
+function Calculator (){
+
+this.getAddNum=function(num1, num2){
+
+return num1+num2
+}
+this.getSubtractNum = function(num1, num2){
+  return num1-num2
+}
+this.getMultiplyNum = function(num1, num2){
+ return num1*num2
+}
+this.getDivideNum = function(num1, num2){
+  return num1/num2
+}
+} const Calculator1 = new Calculator()
+
+alert(Calculator1.getMultiplyNum(1,4))
+
+// Задание 3.
+function Book (){
+  let title_= title
+  let autor_= autor
+  let yer_= yer
+  this.getTitle = function(title){
+    return title
+  }
+  this.getAutor = function(autor){
+    return autor
+  }
+  this.getYer = function(yer){
+    return yer
+  }
+} const Book1 = new Book()
+alert(Book1.getTitle(Potter))
+
+// Задание 4.
+function Cart(){
+  let carts = []
+  this.getAaddltem = function(product){
+    return carts.push(product)
+  }
+this.getRemoveltem = function(product){
+let indexProduct = carts.indexOf(product)
+carts.splice(indexProduct,1)
+}
+  this.getClearCart = function(){
+    carts.splice(0,carts.length)
+  }
+}
+const Cart1 = new Cart()
+Cart1.getClearCart()
+
+// Задание 5.
+function Auto(){
+  let brand_=brand
+  let model_=model
+  let yer_=yer
+  this.setChangeYer = function(yer){
+    return yer
+  }
+}
+const Auto1 = new Auto()
+ alert(Auto1.setChangeYer("BMW"))
